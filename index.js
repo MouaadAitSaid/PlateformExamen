57a73aa487ea3708b0307a54d7667baddc4ecf6f
MouaadAitSaid <aitsaidmouaadoff@gmail.com>
2020-04-03 14:31:58 +0200

    Fixing start script

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   global.log('App is listening on port 3000 ...');
 });
