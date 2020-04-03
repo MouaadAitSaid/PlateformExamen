a93a9509d184c6e22ad0bbc7c0fa4726993da5c7
MouaadAitSaid <aitsaidmouaadoff@gmail.com>
2020-04-03 13:08:15 +0200

    Adding Lint/Nodemon/winston configuration

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { log } = require('./src/configs/winstonConfig');
+
+global.log = log;
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  global.log('App is listening on port 3000!');
+});
